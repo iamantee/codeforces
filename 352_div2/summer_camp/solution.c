4949fe4ac118b555bc2c91ddbd20f36c518ff8fc
Antee <iamantee@gmail.com>
2016-05-21 00:03:29 +0000

    init

---
+++ 352_div2/summer_camp/solution.c
@@ -0,0 +1,29 @@
+/* *
+ * author     : antee
+ * date       : 2016-05-19 22:46
+ * description: This program is a solution for the "Summer Camp" problem on codeforces.
+ *              To access the problem, please click this link: http://codeforces.com/problemset/problem/672/A
+ * */
+ 
+#include<stdio.h>
+
+int main(int argc, char** argv){
+  if(argc < 0) return -1;
+  else if(argc == 2){
+    int position = argv[1];
+    int digit;
+  }
+  else{
+    printf("Position inputted incorrectly.");
+    return -1;
+  }
+
+  if(position < 9){
+    digit = position;
+  }
+  else if(position )
+}
+
+
+
+ 
